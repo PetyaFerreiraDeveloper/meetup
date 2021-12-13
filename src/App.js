9d49ee7c2b7f0fe310b463e97ec1fc0e2316dfe9
Petya Ferreira <petianaidenova@gmail.com>
2021-12-13 13:40:36 +0100

    finish up the meetup application

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavouritesPage from "./pages/Favorites";
+import Layout from './components/layout/Layout';
+
 
 function App() {
+  // domain is: localhost: 3000/favourites
+  // after deployment domain is: my-page.com
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout> 
+      <Routes>
+        <Route path="/" exact element={<AllMeetupsPage />} />
+
+        <Route path="/new-meetup" element={<NewMeetupPage />} />
+
+        <Route path="/favorites" element={<FavouritesPage />} />
+      </Routes>
+    </Layout>
   );
 }
 
